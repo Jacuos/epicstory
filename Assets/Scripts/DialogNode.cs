846d642cf2cac1b35902e2d3eabcda94e6c2d077
Jacek <jacek.kozieja@gmail.com>
2017-06-14 14:18:36 +0200

    Initial commit

---
+++ Assets/Scripts/DialogNode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class DialogNode {
+
+	public string name;
+	public string message;
+	public Dictionary<string, DialogNode> neighbourList;
+
+	public DialogNode()
+	{
+		name = "";
+		message = "";
+		neighbourList = new Dictionary<string, DialogNode>();
+	}
+
+	public DialogNode(string nn, string mm)
+	{
+		name = nn;
+		mm = mm.Replace("]]","");
+		mm = mm.Replace("\n", "");
+		string[] splitted = Regex.Split(mm,"\\[\\[");
+		message = splitted[0];
+		neighbourList = new Dictionary<string, DialogNode>();
+		//Debug.Log("NAME: "+name);
+		//Debug.Log("MESSAGE: "+message);
+		for (int i = 1; i < splitted.Length; i++ )
+		{
+			neighbourList.Add(splitted[i],new DialogNode());
+			Debug.Log("OPTION: " + splitted[i]);
+		}
+	}
+}
---
+++ Assets/Scripts/NPCLoadDialog.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using UnityEngine;
+
+public class NPCLoadDialog : MonoBehaviour 
+{
+	public string twineFileName;
+
+
+	private Dictionary<string, DialogNode> dialog = new Dictionary<string, DialogNode>();
+	void Start () 
+	{
+		string path = System.IO.Path.Combine(Application.streamingAssetsPath, twineFileName);
+		string lines;
+		using (StreamReader sr = new StreamReader(path))
+		{
+			lines = sr.ReadToEnd();
+		}
+		int index = lines.IndexOf("<tw-storydata");
+		int index2 = lines.IndexOf("</tw-storydata>");
+		lines = lines.Substring(index, index2 - index + "</tw-storydata>".Length);
+		int index3 = lines.IndexOf("hidden");
+		lines = lines.Remove(index3, "hidden".Length);
+		Debug.Log(lines);
+
+		XmlReader reader = XmlReader.Create(new StringReader(lines));
+		bool isPassageNode = false;
+		string nodeName = "";
+		while (reader.Read())
+		{
+			if (reader.Name == "tw-passagedata" && reader.NodeType == XmlNodeType.Element)
+			{
+				nodeName = reader.GetAttribute("name");
+				isPassageNode = true;
+			}
+			else if(isPassageNode)
+			{
+				//Debug.Log(nodeName +": "+reader.Value);
+				dialog.Add(nodeName, new DialogNode(nodeName, reader.Value));
+				isPassageNode = false;
+				nodeName = "";
+			}
+
+			/*switch (reader.NodeType)
+			{
+				case XmlNodeType.Element: // The node is an element.
+					Debug.Log("<" + reader.Name + ">");
+					break;
+				case XmlNodeType.Text: //Display the text in each element.
+					Debug.Log(reader.Value);
+					break;
+				case XmlNodeType.EndElement: //Display the end of the element.
+					Debug.Log("</" + reader.Name + ">");
+					break;
+			}*/
+		}
+		reader.Close();
+		ConnectNodes();
+	}
+
+	private void ConnectNodes()
+	{
+		List<string> nodes = new List<string>(dialog.Keys);
+		foreach (string node in nodes)
+		{
+			Debug.Log("DICT-STATE: " + node);
+			List<string> neighbours = new List<string>(dialog[node].neighbourList.Keys);
+			foreach(string neighbour in neighbours)
+			{
+				dialog[node].neighbourList[neighbour] = dialog[neighbour];
+			}
+		}
+
+		foreach (var node in dialog.Values)
+		{
+			Debug.Log(node.name + ": "+ node.message);
+			foreach (KeyValuePair<string, DialogNode> neighbour in node.neighbourList)
+			{
+				Debug.Log("LINK: "+neighbour.Key+" ->"+neighbour.Value.message);
+			}
+		}
+	}
+	
+}
